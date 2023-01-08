c291c6d38403aaeaa2d55bb6daf471b55365e07f
Mditran <micheldiaz@unicauca.edu.co>
2023-01-07 20:30:02 -0500

    Se ha agragado nounce value aplicando Proof of Work

--- main.js
+++ main.js
@@ -1,15 +1,9 @@
 const BlockChain = require("./BlockChain");
 const Block = require("./Block");
 
-const UniCoin = new BlockChain();
+let UniCoin = new BlockChain();
+console.log('Mining block 1...');
 UniCoin.addBlock(new Block(1, "20/07/2017", { amount: 4 }));
+
+console.log('Mining block 2...');
 UniCoin.addBlock(new Block(2, "20/07/2017", { amount: 8 }));
-
-
-console.log('Blockchain valid? ' + UniCoin.isChainValid());
-
-console.log('Changing a block...');
-UniCoin.chain[1].data = { amount: 100 };
-// UniCoin.chain[1].hash = UniCoin.chain[1].calculateHash();
-
-console.log("Blockchain valid? " + UniCoin.isChainValid());
